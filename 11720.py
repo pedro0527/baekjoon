8fb0f7adf7fe54d38bb8fecaf83d961756b776c5
pedro0527 <sooyong0527@naver.com>
2024-10-03 16:34:26 +0900

    feat : sum함수 사용해서 쉽게 구할 수 있음

---
+++ 11720.py
@@ -0,0 +1,13 @@
+N = int(input())
+num = input()
+sum = 0
+
+for i in range(len(num)):
+  sum += int(num[i])
+
+# print(sum)
+
+# num = input()
+# numbers = list(map(int,input()))
+
+# print(sum(numbers))
